bd6ae7f10c593ec27f83e159e4b0357dd37c5d25
qqwqqw689 <114795525+qqwqqw689@users.noreply.github.com>
2023-10-06 22:02:13 +0800

    updates

--- kernel/spinlock.h
+++ kernel/spinlock.h
@@ -1,6 +1,8 @@
 // Mutual exclusion lock.
 struct spinlock {
   uint locked;       // Is the lock held?
+  // zero -> the lock is available.
+  // non-zero -> the lock is not available.
 
   // For debugging:
   char *name;        // Name of lock.
